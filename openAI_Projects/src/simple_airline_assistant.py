8d9837950394d6da70a6ac22c09cf91172cf7b7c
Anuj Singhal <62522752+anuj-singhal@users.noreply.github.com>
2025-05-27 10:48:44 +0400

    added airline assistant chatbot with tool

--- openAI_Projects/src/simple_airline_assistant.py
+++ openAI_Projects/src/simple_airline_assistant.py
@@ -7,19 +7,17 @@ load_dotenv(override=True)
 def main():
     openai = OpenAI()
     model = "gpt-4o-mini"
-    system_message = "You are an helpful assistant that can help to solve mathmatics problems."
+    system_message = "You are a helpful assistant for an Airline called FlightAI. "
+    system_message += "Give short, courteous answers, no more than 1 sentence. "
+    system_message += "Always be accurate. If you don't know the answer, say so."
+    
     def chat(message, history):
         messages = [{"role": "system", "content": system_message}] + history + [{"role": "user", "content": message}]
 
-        steam = openai.chat.completions.create(model=model, 
+        response = openai.chat.completions.create(model=model, 
                                                messages=messages, 
-                                               stream=True)
-        
-        response = ""
-
-        for chunk in steam:
-            response += chunk.choices[0].delta.content or ''
-            yield response
+                                               )
+        return response.choices[0].message.content
 
     gr.ChatInterface(fn=chat, type="messages").launch()
 
